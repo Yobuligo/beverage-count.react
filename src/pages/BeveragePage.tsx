94deaf1def2d751c8202859e77aa5ce50b3b90c0
Peter <hoffmannpeter82@gmail.com>
2023-06-17 22:19:59 +0200

    Load beverages and consumptions when launching BeveragePage

--- src/pages/BeveragePage.tsx
+++ src/pages/BeveragePage.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { BeverageDAO } from "../api/BeverageDAO";
+import { ConsumptionDAO } from "../api/ConsumptionDAO";
 import { AppContext } from "../context/AppContext";
 import { BeverageCardList } from "../features/beverage/list/BeverageCardList";
 import { useInitialize } from "../hooks/useInitialize";
@@ -8,8 +9,12 @@ export const BeveragePage: React.FC = () => {
   const context = useContext(AppContext);
 
   useInitialize(async () => {
-    const beverages = await BeverageDAO.findAll();
-    context.beverages.setDataObjects(beverages);
+    const data = await Promise.all([
+      BeverageDAO.findAll(),
+      ConsumptionDAO.findAll(),
+    ]);
+    context.beverages.setDataObjects(data[0]);
+    context.consumptions.setDataObjects(data[1]);
   });
 
   return <BeverageCardList />;
