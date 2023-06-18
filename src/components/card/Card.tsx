3e5d066984cefce6a4967e704eeccfd77195511c
Peter <hoffmannpeter82@gmail.com>
2023-06-18 14:53:13 +0200

    Add border for BeverageDelete

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,5 +2,7 @@ import styles from "./Card.module.css";
 import { ICardProps } from "./ICardProps";
 
 export const Card: React.FC<ICardProps> = (props) => {
-  return <div className={styles.card}>{props.children}</div>;
+  return (
+    <div className={`${styles.card} ${props.className}`}>{props.children}</div>
+  );
 };
