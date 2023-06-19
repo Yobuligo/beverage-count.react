6c65e586fabc2dafc42a5143836f04595263dc6b
Peter <hoffmannpeter82@gmail.com>
2023-06-19 14:45:19 +0200

    Extend Error Boundary by printing error details to console

--- src/features/errorBoundary/ErrorBoundary.tsx
+++ src/features/errorBoundary/ErrorBoundary.tsx
@@ -1,7 +1,23 @@
-import { Component, ReactNode } from "react";
+import { Component, ErrorInfo, ReactNode } from "react";
+import { IErrorBoundaryProps } from "./IErrorBoundaryProps";
+import { IErrorBoundaryState } from "./IErrorBoundaryState";
 
-export class ErrorBoundary extends Component {
+export class ErrorBoundary extends Component<
+  IErrorBoundaryProps,
+  IErrorBoundaryState
+> {
+  state: IErrorBoundaryState = { hasError: false };
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.log(error.message);
+    console.log(error.stack);
+    console.log(errorInfo.componentStack);
+    this.setState({ hasError: true });
+  }
+
   render(): ReactNode {
-    return <></>;
+    return (
+      <>{this.state.hasError ? <p>Error occurred.</p> : this.props.children}</>
+    );
   }
 }
