1f2ecb22ad5fba7cca90e8b6f6e94f20939d8739
Peter <hoffmannpeter82@gmail.com>
2023-06-20 09:42:44 +0200

    Adjust useLocalStorage, fallback value is also persisted

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,8 +1,11 @@
 import { useCallback, useState } from "react";
 import { readLocalStorage } from "../utils/localStorage/readLocalStorage";
+import { writeLocalStorage } from "../utils/localStorage/writeLocalStorage";
 
 export const useLocalStorage = <T>(key: string, fallback: T) => {
-  const [value, setValue] = useState(readLocalStorage(key) ?? fallback);
+  const [value, setValue] = useState(
+    readLocalStorage(key) ?? writeLocalStorage(key, fallback)
+  );
 
   const updateValue = useCallback(
     (newValue: T) => {
--- src/utils/localStorage/writeLocalStorage.ts
+++ src/utils/localStorage/writeLocalStorage.ts
@@ -1,4 +1,5 @@
-export const writeLocalStorage = <T>(key: string, data: T) => {
+export const writeLocalStorage = <T>(key: string, data: T): T => {
   const item = JSON.stringify(data);
   localStorage.setItem(key, item);
+  return data;
 };
